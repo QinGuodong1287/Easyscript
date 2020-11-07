b0c9a953465a180fa3a8c6dca36e72f1ac1803e7
Qin Guodong <qinguodong07@qq.com>
2020-11-07 16:52:01 +0800

    Add files via upload

--- Syntax/tools/contorl.py
+++ Syntax/tools/contorl.py
@@ -1,18 +1,21 @@
 import sys
 from subprocess import Popen
+from atexit import register
 
 import cursor_contorl
 
 cursor_contorl.hide_cursor()
-cursor_contorl.clear()
-
-def quit() -> int:
-    cursor_contorl.show_cursor()
-    return sys.exit(0)
 
 def execute(command: str) -> int:
     Popen(command, shell=True, stdin=sys.stdin, stdout=sys.stdout, stderr=sys.stderr).wait()
     return 0
+
+execute("clear")
+
+def quit() -> int:
+    execute("clear")
+    cursor_contorl.show_cursor()
+    return sys.exit(0)
 
 def move(direction: str) -> int:
     if direction in ("up", "down", "left", "right"):
--- Syntax/tools/install_setup.py
+++ Syntax/tools/install_setup.py
@@ -1,8 +1,13 @@
 from distutils.core import setup
-import os, sys
+from subprocess import Popen
+import os
+from sys import stdin, stdout, stderr
 from Cython.Build import cythonize
 
 setup(
     name="cursor_contorl", 
-    ext_modules=cythonize("cursor_contorl.py")
+    ext_modules=cythonize(
+        os.path.dirname(os.path.abspath(__file__)) + "/source_codes/cursor_contorl.py"
+    )
 )
+Popen("mv cursor_contorl.c source_codes/", shell=True, stdin=stdin, stdout=stdout, stderr=stderr).wait()
