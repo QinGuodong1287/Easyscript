85506d2342adfb356ce2e6744b37bc7e7d5f88fc
Qin Guodong <qinguodong07@qq.com>
2020-11-09 11:21:26 +0800

    Add files via upload

--- Syntax/setup.py
+++ Syntax/setup.py
@@ -3,9 +3,10 @@ import os, sys
 def show_cursor() -> None:
     print("\033[?25h")
 
-file_path = os.path.dirname(os.path.abspath(__file__))
-from tools import reader, contorl
-build_ext_path = file_path + "/tools"
+from tools import reader, contorl, settings
+program_path = os.path.dirname(os.path.abspath(__file__))
+build_ext_path = program_path + "/tools"
+error_file_path = program_path + "/error.txt"
 try:
     from tools import cursor_contorl # Import the extension
 except ImportError: # If import is failed
@@ -28,7 +29,6 @@ except ImportError: # If import is failed
     del pwd
 contorl.execute("clear")
 
-text_path = file_path + "/Syntax_manaul/CN"
 keylayouts = {
     "q": contorl.quit,       # Quit the manaul
     "j": contorl.move_down,  # Go to down text
@@ -37,16 +37,22 @@ keylayouts = {
     "l": contorl.move_right  # Go to next text
 }
 keys = tuple(keylayouts.keys())
-
-def main() -> int:
-    print("""Welcome to the Syntax manaul for Easyscript programming language.
+welcome_text = f"""{settings.title}
+Welcome to the Syntax manaul for Easyscript programming language.
 Press any key to read this manaul.
 Keys:
 q: quit this manaul
 j: move the page down.
 k: move the page up.
 h: read pervious text.
-l: read next text.""")
+l: read next text."""
+text_list = welcome_text.splitlines()
+text_list[0] = f"{text_list[0]}\033[7m{' ' * (os.get_terminal_size().columns - len(text_list[0]) + 8)}\033[0m"
+welcome_text = '\n'.join(text_list)
+del text_list
+
+def main() -> int:
+    print(welcome_text)
     while True:
         ch = reader.get_character().lower()
         if ch not in keys:
@@ -56,16 +62,13 @@ l: read next text.""")
             keylayouts[ch]()
         except SystemExit: # Exit this manual
             break
-        except:
-            pass
-        print("""Welcome to the Syntax manaul for Easyscript programming language.
-Press any key to read this manaul.
-Keys:
-q: quit this manaul
-j: move the page down.
-k: move the page up.
-h: read pervious text.
-l: read next text.""")
+        except Exception as e:
+            if os.path.exists(error_file_path):
+                with open(error_file_path, 'a') as fp:
+                    fp.write(str(e) + '\n')
+            else:
+                with open(error_file_path, 'w') as fp:
+                    fp.write(str(e) + '\n')
     return 0
 
 if __name__ == "__main__":
